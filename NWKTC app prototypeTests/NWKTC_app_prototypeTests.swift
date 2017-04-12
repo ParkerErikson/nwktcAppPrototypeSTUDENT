e7898326d90a4b27d47683d77fa4f43e2d2a09ec
Parker Erikson <parkererikson@maed-2.local>
2017-04-12 10:00:16 -0600

    Initial Commit

---
+++ NWKTC app prototypeTests/NWKTC_app_prototypeTests.swift
@@ -0,0 +1,36 @@
+//
+//  NWKTC_app_prototypeTests.swift
+//  NWKTC app prototypeTests
+//
+//  Created by Parker Erikson on 4/12/17.
+//  Copyright © 2017 Parker Erikson. All rights reserved.
+//
+
+import XCTest
+@testable import NWKTC_app_prototype
+
+class NWKTC_app_prototypeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
