1b043fb578712e706ad7a24b473df3cd6639073e
Zachary White <zacharywhite@maed-74.local>
2017-04-20 11:06:03 -0600

    Deleted SelectedVC, Working on HomeVC including SelectedVC

---
+++ NWKTC app prototype/HomeTableViews.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViews.swift
+//  NWKTC app prototype
+//
+//  Created by Zachary White on 4/20/17.
+//  Copyright © 2017 Parker Erikson. All rights reserved.
+//
+
+import UIKit
+
+class SectionCells: UITableViewCell {
+    @IBOutlet weak var sectionLbl: UILabel!
+    
+}
+
+class SelectedCells: UITableViewCell {
+    @IBOutlet weak var selectedLbl: UILabel!
+    
+    
+}
--- NWKTC app prototype/HomeViewController.swift
+++ NWKTC app prototype/HomeViewController.swift
@@ -9,37 +9,64 @@
 import UIKit
 
 class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var sectionsTblView: UITableView!
+    @IBOutlet weak var selectedTblView: UITableView!
+    
     lazy var sections = ["News","Admin", "Automotive Technology", "Business Technology", "Carpentry", "Collision Repair", "Computer Graphics Technology", "Cosmetology", "Crime Scene Investigation", "Kansas Institute of Diesel Technology", "Electrical Technology", "Engineering and Drafting Technology", "Medical Assistant", "Mobile App and Gaming", "Network Technology", "Precision Agriculture Technology", "Respiratory Therapy", "Welding"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sections.count
+        
+        if (tableView == sectionsTblView) {
+            return sections.count
+        }
+        else {
+            return 5
+        }
+        
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "sectionCell", for: indexPath)
         
-        // Configure the cell...
+        if (tableView == sectionsTblView) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "sectionCell", for: indexPath) as! SectionCells
+            
+            cell.textLabel?.text = sections[indexPath.row]
+            cell.textLabel?.textColor = #colorLiteral(red: 0.8532782197, green: 0.1581068337, blue: 0.194409132, alpha: 1)
+            cell.textLabel?.numberOfLines = 2
+            
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "selectedCell", for: indexPath) as! SelectedCells
+            
+            cell.textLabel?.text = "Pie"
+            cell.textLabel?.textColor = #colorLiteral(red: 0.8532782197, green: 0.1581068337, blue: 0.194409132, alpha: 1)
+            cell.textLabel?.numberOfLines = 2
+            
+            return cell
+        }
         
-        cell.textLabel?.text = sections[indexPath.row]
-        cell.textLabel?.textColor = #colorLiteral(red: 0.8532782197, green: 0.1581068337, blue: 0.194409132, alpha: 1)
-        cell.textLabel?.numberOfLines = 2
         
-        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("\(sections[indexPath.row])")
+        if (tableView == sectionsTblView) {
+            print("\(sections[indexPath.row])")
+        } else {
+            print("pie")
+        }
+        
     }
     
 }
